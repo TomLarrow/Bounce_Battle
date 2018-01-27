6f10f20e9927484993b14adf6d15228db0776d81
Tom Larrow <tomlarrow@gmail.com>
2018-01-27 15:09:34 -0500

    dynamically coloring pieces

--- Balls/Ball.gd
+++ Balls/Ball.gd
@@ -2,7 +2,12 @@ extends RigidBody2D
 
 export (int) var ball_speed
 
+onready var score=.5
+onready var ball_texture = get_node("Ball_Sprite")
+
 func _ready():
+	var score_color = Color(1.0-score,0.5,score,1)
+	ball_texture.modulate = score_color
 	var initial_velocity = Vector2( rand_range(-1, 1), rand_range( -1, 1)).normalized()
 	apply_impulse(Vector2(), initial_velocity * ball_speed)
 
--- Players/Player.gd
+++ Players/Player.gd
@@ -1,7 +1,12 @@
 extends RigidBody2D
 
 export (int) var ball_speed
+export (float) var score
 
 func _ready():
+	
+	var score_color = Color(1.0-score,0.5,score,1)
+	get_node("Player_Sprite").modulate = score_color
+	
 	var initial_velocity = Vector2( rand_range(-1, 1), rand_range( -1, 1)).normalized()
 	apply_impulse(Vector2(), initial_velocity * ball_speed)
